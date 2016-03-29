4ed56341f4bb71ad42139d664988652ef7caf48e
Chandru Naik <cmnaik007@gmail.com>
2016-03-29 11:58:07 +0530

    Initial commit, Add isDefined function

--- typecheck.js
+++ typecheck.js
@@ -1,1 +1,16 @@
-
+/**
+  * window - global object
+  * function to check all the data types in javascript
+  */
+(function(window){
+  var typeCheck = {};
+  
+  // check variable is defined or not
+  // return true if defined, else false
+  typeCheck.isDefined = function(val){
+    return Object.prototype.toString.call(val) === "[object Undefined]";
+  };
+  
+  //export the lib as a global object of window
+  return window.typeCheck = typeCheck;
+})(window);
