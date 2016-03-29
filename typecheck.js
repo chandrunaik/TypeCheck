027572c7201fe5fc4fc06de677a5f43c431f5260
chandrunaik <cmnaik007@gmail.com>
2016-03-29 12:30:51 +0530

    Modified comments

--- typecheck.js
+++ typecheck.js
@@ -1,16 +1,20 @@
 /**
-  * window - global object
-  * function to check all the data types in javascript
-  */
-(function(window){
-  var typeCheck = {};
-  
-  // check variable is defined or not
-  // return true if defined, else false
-  typeCheck.isDefined = function(val){
-    return Object.prototype.toString.call(val) === "[object Undefined]";
-  };
-  
-  //export the lib as a global object of window
-  return window.typeCheck = typeCheck;
+ * library to check all data types of javascript
+ * @param  {[global object]} window [global object]
+ * @return {[global object]}        [Return library as a global object]
+ */
+(function(window) {
+    var typeCheck = {};
+
+    /**
+     * check variable is defined or not
+     * @param  {[any]}  val [any data type]
+     * @return {Boolean}     [return true is defined else false]
+     */
+    typeCheck.isDefined = function(val) {
+        return Object.prototype.toString.call(val) === "[object Undefined]";
+    };
+
+    //export the lib as a global object of window
+    return window.typeCheck = typeCheck;
 })(window);
